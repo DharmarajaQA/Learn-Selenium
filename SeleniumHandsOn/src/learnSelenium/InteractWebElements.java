09860111723dd568749375244e216c4568a7ff4e
Dharmaraja N N <Dharmaraja N N@192.168.0.28>
2021-07-11 18:25:08 +0530

    Initial commit

---
+++ SeleniumHandsOn/src/learnSelenium/InteractWebElements.java
@@ -0,0 +1,57 @@
+package learnSelenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class InteractWebElements {
+
+	static WebDriver driver = null;
+
+	public static void openBrowser() {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\dharm\\Downloads\\Webdriver\\Chrome\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("http://www.leafground.com/");
+		driver.manage().window().maximize();
+	}
+
+	public static void verifyTextField() throws InterruptedException {
+		driver.findElement(By.linkText("Edit")).click();
+		Thread.sleep(5000);
+
+		//Enter value to the text field
+		driver.findElement(By.id("email")).sendKeys("selenium@gmail.com");	
+
+		//Clear value from the text field
+		driver.findElement(By.xpath("//input[@name='username' and @value ='Clear me!!']")).clear();
+
+		//Verify Text field is enabled
+		WebElement disabledField = driver.findElement(By.xpath("//input[@disabled='true']"));
+		if(disabledField.isEnabled()) {
+			System.out.println("Text field is enabled");
+
+		}else
+			System.out.println("Text field is disabled");
+		
+		//Get Text from Default field
+		WebElement defaultField =driver.findElement(By.xpath("//input[@name='username' and @value='TestLeaf']"));
+		String string = defaultField.getText().toString();
+		System.out.println("Default Text value is "+string);
+	}
+
+	public static void tearDown() {
+		driver.quit();
+
+	}
+
+
+	public static void main(String[] args) throws InterruptedException {
+		openBrowser();
+		verifyTextField();
+		Thread.sleep(3000);
+		tearDown();
+
+	}
+}
---
+++ SeleniumHandsOn/src/learnSelenium/OpenBrowser.java
@@ -0,0 +1,54 @@
+package learnSelenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class OpenBrowser {
+	static WebDriver driver = null;
+
+	public static void chromeBrowser() {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\dharm\\Downloads\\Webdriver\\Chrome\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("https://www.google.co.in/");
+		driver.manage().window().maximize();
+
+	}
+
+	public static void edgeBrowser() {
+		System.setProperty("webdriver.edge.driver", "C:\\Users\\dharm\\Downloads\\Webdriver\\Edge\\msedgedriver.exe");
+		driver = new EdgeDriver();
+		driver.get("https://www.google.co.in/");
+		driver.manage().window().maximize();
+
+	}
+
+	public static void tearDown() {
+		driver.quit();
+
+	}
+	
+	public static void searchOperation() {
+		
+		driver.findElement(By.name("q")).sendKeys("Chennai"+Keys.ENTER);
+	}
+
+	public static void main(String[] args) throws InterruptedException {
+
+		// Launching Chrome browser
+		chromeBrowser();
+		searchOperation();
+		Thread.sleep(3000); 
+		tearDown();
+		
+		// Launching Edge browser
+		edgeBrowser();
+		searchOperation();
+		Thread.sleep(3000); 
+		tearDown();
+
+	}
+
+}
